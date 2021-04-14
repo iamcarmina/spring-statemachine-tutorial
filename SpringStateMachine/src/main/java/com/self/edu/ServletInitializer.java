106cf82a56d230ee99e6742184049bea1dde07ec
Nina Carmina <i.am.carmina@gmail.com>
2021-04-14 15:45:00 +0800

    first cut

---
+++ SpringStateMachine/src/main/java/com/self/edu/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.self.edu;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringStateMachineApplication.class);
+	}
+
+}
